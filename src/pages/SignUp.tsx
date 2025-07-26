0847edcab0b97622f6b69dda8f4a404e5d41521c
KAMAL20201 <arorakamal706@gmail.com>
2025-07-26 12:38:12 +0530

    Fix: add redirect UrL

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -91,9 +91,9 @@ export default function SignupPage() {
         case "Google":
           authResult = await supabase.auth.signInWithOAuth({
             provider: "google",
-            // options: {
-            //   redirectTo: `${window.location.origin}/auth/callback`,
-            // },
+            options: {
+              redirectTo: `${window.location.origin}/`,
+            },
           });
           break;
         default:
