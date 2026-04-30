2aef29758e22ab65561afc2ee233461dc659f66e
KAMAL20201 <arorakamal706@gmail.com>
2026-04-30 21:09:35 +0530

    feat: increase cache duration for improved performance

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -110,7 +110,7 @@ export async function loader(_: Route.LoaderArgs) {
     },
     {
       headers: {
-        "Cache-Control": "public, s-maxage=60, stale-while-revalidate=300",
+        "Cache-Control": "public, s-maxage=300, stale-while-revalidate=3600",
       },
     },
   );
